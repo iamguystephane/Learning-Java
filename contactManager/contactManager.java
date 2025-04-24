ce38d1bfd957cd6620ab7f24130261c3c924a970
iamguystephane <gstephane138@gmail.com>
2025-04-24 19:24:53 +0100

    file handling learnt

--- contactManager/contactManager.java
+++ contactManager/contactManager.java
@@ -5,21 +5,27 @@ public class contactManager {
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
         try {
-            FileWriter writer = new FileWriter("contactManager/contacts.txt", true);
+            FileWriter writer = new FileWriter("contactManager/address.txt", true);
             System.out.print("Enter name: ");
             String name = scanner.nextLine();
             System.out.print("Enter phone: ");
             String phone = scanner.nextLine();
-            writer.write(name + ", " + phone + "\n");
+            System.out.print("Enter email: ");
+            String email = scanner.nextLine();
+            System.out.print("Enter address: ");
+            String address = scanner.nextLine();
+            System.out.print("Enter date of birth: ");
+            String dob = scanner.nextLine();
+            writer.write(name + ", " + phone + ", " + email + ", " + address + ", " + dob + "\n");
             writer.close();
-            System.out.println("Contact saved!");
+            System.out.println("Adress information saved!");
 
-            System.out.println("\nAll Contacts: ");
-            BufferedReader reader = new BufferedReader(new FileReader("contactManager/contacts.txt"));
+            System.out.println("\nAll Information: ");
+            BufferedReader reader = new BufferedReader(new FileReader("contactManager/address.txt"));
             String line;
             while((line = reader.readLine()) !=null ) {
                 String[] parts = line.split(",");
-                System.out.println("Name: " + parts[0] + ", Phone: " + parts[1]);
+                System.out.println("Name: " + parts[0] + ", Phone: " + parts[1] + ", Email: " + parts[2] + ", Address: " + parts[3] + ", DOB: " + parts[4]);
             }
             reader.close();
         } catch (IOException e) {
